476284163e2c1edb19b8ac666e67bdf98fd0980a
M Sathya Kumar <56182934+Sathyakumar-M@users.noreply.github.com>
2022-08-24 14:54:22 +0530

    new

--- src/main/java/com/cognizant/microservices/pensionerprocessmicrcoservice/authclient/AuthorizationClient.java
+++ src/main/java/com/cognizant/microservices/pensionerprocessmicrcoservice/authclient/AuthorizationClient.java
@@ -3,11 +3,12 @@ package com.cognizant.microservices.pensionerprocessmicrcoservice.authclient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-@FeignClient(name = "authorizationService",url = "http://localhost:9090")
-//@FeignClient(name = "authorizationService",url = "${AUTHSERVICEURL}=http://localhost:9090")
+//@FeignClient(name = "authorizationService",url = "http://localhost:9090")
+@FeignClient(name = "authorizationService",url = "${AUTHSERVICEURL:http://localhost:9090}")
 public interface AuthorizationClient {
 	
 	@GetMapping("/authorize")
 	public Boolean authorization(@RequestHeader("Authorization") String token);
+	
 
 }
